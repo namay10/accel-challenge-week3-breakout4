58d00bb3a99b15d73c6132f5f7c8ee929ac0a72e
Namay Gupta <namaygupta@Namays-MacBook-Air.local>
2026-02-21 23:51:04 +0530

    added init file

---
+++ src/commands/init.rs
@@ -0,0 +1,16 @@
+use std::{env, fs};
+
+pub fn init() -> Result<(), std::io::Error> {
+    let repo_path = env::current_dir()?.join(".it");
+    if repo_path.exists() && repo_path.is_dir() {
+        println!("Already a IT Repository");
+        return Ok(());
+    }
+    fs::create_dir_all(repo_path.join("objects"))?;
+    fs::create_dir_all(repo_path.join("refs/heads"))?;
+    fs::write(repo_path.join("HEAD"), format!("ref: refs/heads/main\n"))?;
+
+    //for it add command
+    fs::write(repo_path.join("index"), format!("it-index"))?;
+    Ok(())
+}
